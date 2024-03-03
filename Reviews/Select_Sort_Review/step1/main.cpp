c7cf12d99df260a15e90793536f5b1807d76d6ca
Kobe <dev.skyachieve91@gmail.com>
2024-03-03 15:06:19 +0900

    :sparkles: Feat: '선택 정렬 part1 복습'

--- Reviews/Select_Sort_Review/step1/main.cpp
+++ Reviews/Select_Sort_Review/step1/main.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 bool CheckSorted(int* arr, int size) {
 	// TODO: 정렬 확인 함수 구현
-
+	for (int i = 0; i < size -1; i++) {
+		if (arr[i] <= arr[i + 1]) {
+			return true;
+		}
+	}
 	return false;
 }
 
@@ -23,8 +27,26 @@ int main() {
 					}
 
 					cout << " -> " << flush;
-					
+
 					// TODO: 정렬하기
+					if (arr[0] > arr[1]) {
+						int temp = arr[0];
+						arr[0] = arr[1];
+						arr[1] = temp;
+					}
+
+					if (arr[1] > arr[2]) {
+						int temp = arr[1];
+						arr[1] = arr[2];
+						arr[2] = temp;
+					}
+
+					if (arr[0] > arr[1]) {
+						int temp = arr[0];
+						arr[0] = arr[1];
+						arr[1] = temp;
+					}
+					
 
 					for (int e = 0; e < size; e++) {
 						cout << arr[e] << " " << flush;
