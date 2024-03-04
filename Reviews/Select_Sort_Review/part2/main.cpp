cbecc17b0891a1ac2c0bd83f71de99277b0d1dec
Kobe <dev.skyachieve91@gmail.com>
2024-03-04 16:28:57 +0900

    :sparkles: Feat: 선택 정렬 part2 복습.

--- Reviews/Select_Sort_Review/part2/main.cpp
+++ Reviews/Select_Sort_Review/part2/main.cpp
@@ -1,63 +1,101 @@
 #include <iostream>
+#include <cassert>
+#include <stdio.h>
 
 using namespace std;
 
 bool CheckSorted(int* arr, int size) {
-	// TODO: 정렬 확인 함수 구현
-	for (int i = 0; i < size-1; i++) {
-		if (arr[i] > arr[i +1]) {
+	for (int i = 0; i < (size - 1); i++) {
+		if (arr[i] > arr[i + 1]) {
 			return false;
-		} else {
-			return true;
 		}
 	}
-	
+	return true;
+}
 
-	return false;
+void Print(int* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+		cout << endl;
+	}
 }
 
 int main() {
-	// 3개 정렬
+	// 5개라면? 더 많다면?
 	{
-		for (int k = 0; k < 3; k++)
-			for (int j = 0; j < 3; j++)
-				for (int i = 0; i < 3; i++) {
-					
-					int arr[3] = { i, j, k };
-					int size = sizeof(arr) / sizeof(arr[0]);
-					// cout << "i의 크기 => " << sizeof(i) << endl;
-					// cout << "arr의 크기 => " << sizeof(arr) << endl;
-					// cout << "arr[0]의 크기 => " << sizeof(arr[0]) << endl;
-					
-					// size == 3
-					for (int e = 0; e < size; e++) {
-						// 0 == i, 1 == j, 2 == k
-						cout << arr[e] << " " << flush;
-					}
+		// 8 4 2 8 3
+		// TODO: ???
 
-					cout << " -> " << flush;
-					
-					// TODO: 정렬하기
-					if (arr[0] > arr[1]) {
-						swap(arr[0], arr[1]);
-					}
+		// 8 3 2 5 1 1 2 5 8 9
+		// TODO: ???
 
-					if (arr[0] > arr[2]) {
-						swap(arr[0], arr[2]);
-					}
+		// 100개라면?
+	}
 
-					if (arr[1] > arr[2]) {
-						swap(arr[1], arr[2]);
-					}
+	// 가장 작은 수 찾기
+	{
+		int arr[] = { 8, 3, 2, 5, 1, 1, 2, 5, 8, 9 }; // 임의의 숫자들
+		int size = sizeof(arr) / sizeof(arr[0]);
+		
 
-					for (int e = 0; e < size; e++) {
-						// 0 == i, 1 == j, 2 == k
-						cout << arr[e] << " " << flush;
-					}
-					cout << boolalpha;
-					cout << CheckSorted(arr, size); // 정렬되있는지 확인
-					cout << endl;
+		assert(size > 0); //size가 1이상이라고 가정
+
+		int min_number = arr[0]; // 일단 가장 첫 값이 가장 작은 값일 수도 있다고 가정.
+		// TODO:
+		for (int i = 0; i < size; i++) {
+			min_number = arr[i] < min_number ? arr[i] : min_number;
+		}
+
+		cout << "Mininum number is " << min_number << endl;
+	}
+
+	// 가장 작은 수의 인덱스 찾기
+	{
+		int arr[] = { 8, 3, 2, 5, 1, 1, 2, 5, 8, 9 }; // 임의의 숫자들
+		int size = sizeof(arr) / sizeof(arr[0]);
+
+		assert(size > 0); //size가 1이상이라고 가정
+
+		// TODO:
+		int min_index = 0;
+		for (int j = 0; j < size; j++) {
+			if (arr[j] < arr[min_index]) {
+				min_index = j;
+			}
+		}
+
+		cout << "The index of min is " << min_index << endl;	
+		cout << "Minimum number is " << arr[min_index] << endl;
+	}
+
+	// Selection Sort
+	// 힌트: swap()
+	{
+		int arr[] = { 8, 3, 2, 5, 1, 1, 2, 5, 8, 9 }; // 임의의 숫자들
+		int size = sizeof(arr) / sizeof(arr[0]);
+		
+		Print(arr, size);
+
+		int min_index;
+		for (int i = 0; i < size - 1; i++) {
+			// TODO:
+			min_index = i;
+			for (int j = i + 1; j < size; j++) {
+				if (arr[j] < arr[min_index]) {
+					min_index = j;
 				}
+			}
+			swap(arr[i], arr[min_index]);
+			
+
+			Print(arr, size);
+
+			cout << boolalpha;
+			cout << CheckSorted(arr, size);
+			cout << endl;
+		}
+		cout << min_index << endl;
+		cout << arr[min_index] << endl;
 	}
 	return 0;
 }
