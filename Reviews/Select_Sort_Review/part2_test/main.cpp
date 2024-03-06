70fc00b382130078aa302239ebccdaaddee7e0ce
Kobe <dev.skyachieve91@gmail.com>
2024-03-06 13:05:26 +0900

    :sparkles: Feat: '선택 정렬 part2' 복습.

--- Reviews/Select_Sort_Review/part2_test/main.cpp
+++ Reviews/Select_Sort_Review/part2_test/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include <stdio.h>
+#include <experimental/iterator>
 
 using namespace std;
 
@@ -32,19 +33,25 @@ int main() {
 
 		int min_index;
 		for (int i = 0; i < size - 1; i++) {
-			// 0,1
+			// size == 9 -> 8까지 -> 앞에 1자리를 픽스하기 위해 -> 어짜피 마지막 자리는 가장 큰 수라서 괜찮음.
 			// TODO:
 			// cout << "i는 " << i << "입니다" << endl;
 			cout << " " << endl;
 			min_index = i;
 			for (int j = i + 1; j < size; j++) {
-				cout << "i가 " << i << "일 때, " << "j는 " << j << "입니다." << endl;
+				// size == 10 -> 9까지 -> 배열의 모든 요소를 다 돌리기 위해
+				cout << "i(min_index)가 " << i << "일 때, " << "j는 " << j << "입니다." << endl;
 				
 				if (arr[j] < arr[min_index]) {
+					cout << "arr[j] 값은 " << arr[j] << "이고, " << "arr[min_index] 값은 " << arr[min_index] << "이므로";
+					cout << " min_index값은 j가 됩니다 즉, j의 값은 " << j << " 이므로, " << "min_index = j; 와 같이 로직을 만들어야 합니다." << endl;
 					min_index = j;
 				}
 			}
 			swap(arr[i], arr[min_index]);
+			cout << "결과적으로 arr[i]와 arr[min_index] 의 자리가 swap이 되므로, 배열은 ";
+			copy(begin(arr), end(arr), experimental::make_ostream_joiner(cout, " "));
+			cout << " 로 바뀝니다." << endl;
 			
 
 			// Print(arr, size);
