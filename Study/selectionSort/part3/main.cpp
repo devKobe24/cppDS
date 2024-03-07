a8125f54c7c3d0a4886c5b84ea6fb35fc0f5f2fe
Kobe <dev.skyachieve91@gmail.com>
2024-03-07 16:47:37 +0900

    :sparkles: Feat: '선택 정렬 part 3 - 안정성 확인.' 학습.

--- Study/selectionSort/part3/main.cpp
+++ Study/selectionSort/part3/main.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+struct Element
+{
+	int key;
+	char value;
+};
+
 void Print(int *arr, int size)
 {
 	for (int i = 0; i < size; i++)
@@ -11,6 +17,17 @@ void Print(int *arr, int size)
 		cout << arr[i] << " ";
 		cout << endl;
 	}
+}
+
+void Print(Element* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << arr[i].key << " ";
+	cout << endl;
+
+	for (int i = 0; i < size; i++)
+		cout << arr[i].value << " ";
+	cout << endl;
 }
 
 // 비교 횟수 세보기, 더 효율적인 방법은 없을까?
@@ -50,6 +67,31 @@ int main()
 			delete[] arr;
 		}
 		ofile.close();
+	}
+
+	// 안정성 확인(unstable)
+	{
+		Element arr[] = { {2, 'a'}, {2, 'b'}, {1, 'c' } };
+		int size = sizeof(arr) / sizeof(arr[0]);
+
+		Print(arr, size);
+
+		int min_index;
+		for(int i = 0; i < (size - 1); i++) 
+		{
+			min_index = i;
+			for (int j = (i + 1); j < size; j++)
+			{
+				if (arr[j].key < arr[min_index].key) 
+				{
+					min_index = j;
+				}
+			}
+
+			swap(arr[i], arr[min_index]);
+
+			Print(arr, size);
+		}
 	}
 	return 0;
 }
